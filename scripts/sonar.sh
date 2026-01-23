820242f1b704bb33ae5304b3c748e4e855c82b15
Logan Donley <ldonley@cloudbees.com>
2026-01-23 15:52:29 -0500

    Disabling branch

--- scripts/sonar.sh
+++ scripts/sonar.sh
@@ -4,14 +4,14 @@ if [[ ! -v PROJECT_KEY ]]; then
     echo "You need to set PROJECT_KEY"
     exit 1
 fi
-if [[ ! -v BRANCH ]]; then
-    echo "You need to set BRANCH"
-    exit 1
-fi
+# if [[ ! -v BRANCH ]]; then
+#     echo "You need to set BRANCH"
+#     exit 1
+# fi
 
 sonar-scanner \
     '-Dsonar.exclusions=test_reports/output-snyk-*.json,test_reports/snyk-*-results.html,catalog-info.yaml' \
     '-Dsonar.coverage.exclusions=test_reports/output-snyk-*.json,test_reports/snyk-*-results.html,catalog-info.yaml' \
     -D "sonar.projectKey=$PROJECT_KEY" \
-    -D "sonar.branch.name=$BRANCH" \
     -D "sonar.python.coverage.reportPaths=${REPORT_PATH:-coverage.xml}"
+# -D "sonar.branch.name=$BRANCH" \
